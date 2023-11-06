87f36427a5c1bd4cff094a762bbc43f0a28b8d58
martin <martin.vspalletti@gmail.com>
2023-11-06 12:31:48 -0300

    comentarios

--- src/app/comentarios/comentarios.component.ts
+++ src/app/comentarios/comentarios.component.ts
@@ -1,22 +1,25 @@
-import { Component, inject } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { PacientesService } from "../servicios/pacientes.service";
+import { Comentarios } from "./comentarios";
+import { ComentariosService } from "../servicios/comentarios.service";
 @Component({
   selector: "app-comentarios",
   templateUrl: "./comentarios.component.html",
   styleUrls: ["./comentarios.component.css"],
 })
-export class ComentariosComponent {
+export class ComentariosComponent implements OnInit {
+
+comentarios: Comentarios[] = []
   comentariosForm: FormGroup;
-
   private fb = inject(FormBuilder);
-  private ps = inject(PacientesService);
-
-  constructor() {
+  constructor(private cs:ComentariosService) {
     this.comentariosForm = this.fb.group({
       nombre: ["", Validators.required],
       detalle: ["", Validators.required],
     });
+  }
+  ngOnInit() {
+    this.cs.getAll().subscribe((comentarios) => this.comentarios = comentarios)
   }
 
   addComentario() {}
